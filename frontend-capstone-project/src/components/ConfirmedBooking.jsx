58faa2a24fc0f7179aae8991f5257f1c7ffc4d7c
freeman237 <ym419974@gmail.com>
2024-11-22 04:13:53 +0100

    Update ConfirmedBooking.jsx

--- frontend-capstone-project/src/components/ConfirmedBooking.jsx
+++ frontend-capstone-project/src/components/ConfirmedBooking.jsx
@@ -1,11 +1,10 @@
-// src/components/ConfirmedBooking.jsx
 import React from "react";
 
 const ConfirmedBooking = () => {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Booking Confirmed!</h1>
-      <p>Thank you for your reservation. We look forward to serving you!</p>
+      <p>Thank you for your reservation. We look forward to seeing you!</p>
     </div>
   );
 };
